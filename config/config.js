1ab804b348aab9373b226977ed24358b5be56b3b
prabhu velayutham <prabhu.v@gmail.com>
2014-05-17 15:04:20 -0600

    modified config

--- config/config.js
+++ config/config.js
@@ -1,34 +1,37 @@
 var path = require('path'),
     rootPath = path.normalize(__dirname + '/..'),
     env = process.env.NODE_ENV || 'development';
-
+/*
+  173.194.244.81 --user=relief --password=
+db - reliefdb
+*/
 var config = {
-  development: {
-    root: rootPath,
-    app: {
-      name: 'cogovdev'
+    development: {
+        root: rootPath,
+        app: {
+            name: 'cogovdev'
+        },
+        port: 3000,
+        db: 'mysql://relief:reliefpwd@173.194.244.81/reliefdb'
     },
-    port: 3000,
-    db: 'mysql://root@localhost/cogovdev'
-  },
 
-  test: {
-    root: rootPath,
-    app: {
-      name: 'cogovdev'
+    test: {
+        root: rootPath,
+        app: {
+            name: 'cogovdev'
+        },
+        port: 3000,
+        db: 'mysql://root@localhost/cogovdev_test'
     },
-    port: 3000,
-    db: 'mysql://root@localhost/cogovdev_test'
-  },
 
-  production: {
-    root: rootPath,
-    app: {
-      name: 'cogovdev'
-    },
-    port: 3000,
-    db: 'mysql://root@localhost/cogovdev_production'
-  }
+    production: {
+        root: rootPath,
+        app: {
+            name: 'cogovdev'
+        },
+        port: 3000,
+        db: 'mysql://relief:reliefpwd@173.194.244.81/reliefdb'
+    }
 };
 
 module.exports = config[env];
