2b204479b289f64a817bf840446579639d035b3f
prabhu velayutham <prabhu.v@gmail.com>
2014-05-18 01:58:35 -0600

    testing blank connection in postgres in heroku

--- app.js
+++ app.js
@@ -66,6 +66,6 @@ db
             http.createServer(app).listen(app.get('port'), function() {
                 console.log('Express server listening on port ' + app.get('port'))
             })
-            require("./testdata/all")(db);
+            //require("./testdata/all")(db);
         }
     });
