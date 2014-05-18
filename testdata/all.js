806af7b50e2a7eaa59d8d02290716edcd1590f21
prabhu velayutham <prabhu.v@gmail.com>
2014-05-17 22:04:37 -0600

    added a sample data

--- testdata/all.js
+++ testdata/all.js
@@ -2,6 +2,17 @@
 var db = require('../app/models');
 var Faker = require('./Faker');
 
+var sampleDonorData = [{
+    donorLastName: "May",
+    donorFirstName: "Paul",
+    donorAddress1: "4520 Broadway Street",
+    donorAddress2: "",
+    donorZipCode: "80201",
+    donorState: "CO",
+    donorCity: "Boulder",
+    donorEmail: "paultr@gmail.com",
+    donorMobileNumber: "38346413234"
+}];
 var donorData = function() {
     return {
         donorLastName: Faker.Name.lastName(),
@@ -17,7 +28,7 @@ var donorData = function() {
 };
 var fakeDonorData = [];
 for (var i = 0; i < 10; i++) {
-    fakeDonorData.push(fakeDonorData());
+    fakeDonorDatas.push(fakeDonorData());
 }
 var deliveryData = [];
 var donationCenterData = [];
