31a5d1d9648c386e95c0cea2abb2605866b50412
prabhu velayutham <prabhu.v@gmail.com>
2014-05-17 19:16:22 -0600

    added restful modules

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var express = require('express'),
-  config = require('./config/config');
-    fs = require('fs'),
-    http = require('http'),
-    path = require('path'),
-    db = require('./app/models'),
-    config = require('./config/config'),
-    donor = require('./app/routes/donor'),
-    material = require('./app/routes/material');
+    config = require('./config/config');
+fs = require('fs'),
+http = require('http'),
+path = require('path'),
+config = require('./config/config'),
+donor = require('./app/routes/donor'),
+material = require('./app/routes/material'),
+restful = require('sequelize-restful'),
+db = require('./app/models');
 /*
 orm.db = orm.connect(config.db, function(err, db){
   if(err){
@@ -24,38 +25,46 @@ fs.readdirSync(modelsPath).forEach(function (file) {
 */
 
 var app = express();
+app.use(restful(db.sequelize, {
+    endpoint: '/api',
+    logLevel: 'debug',
+    allowed: new Array('Donor', 'Material')
+}));
 
 require('./config/express')(app, config);
 //require('./config/routes')(app);
-
 // all environments
 app.set('port', process.env.PORT || 3000)
 app.set('views', __dirname + '/views')
 app.set('view engine', 'html');
 // make a custom html template
-app.engine('html', function(str, options){
-    return function(locals){
+app.engine('html', function(str, options) {
+    return function(locals) {
         return str;
     };
 });
-app.use(express.favicon())
-app.use(express.logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded())
-app.use(express.methodOverride())
-app.use(app.router)
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+//app.use(app.router)
 app.use(express.static(path.join(__dirname, 'public')))
+
+
 
 // development only
 if ('development' === app.get('env')) {
     app.use(express.errorHandler())
 }
 
-app.get('/', function(req,res) {
+app.get('/', function(req, res) {
     res.sendfile('./app/views/index.html');
 });
 
-app.post('/materials/create', material.create)
+app.post('/materials/create', material.create);
+
+
 db
     .sequelize
     .sync({
@@ -70,6 +79,3 @@ db
             })
         }
     });
-
-
-
